03b75d5463e67b8de4edb59f92e7f3397ec120a4
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-06-21 02:18:29 +0100

    refactor: Improve title-casing function and usage

--- plugins/featartistsintitles/featartistsintitles.py
+++ plugins/featartistsintitles/featartistsintitles.py
@@ -10,6 +10,15 @@ import re
 _feat_re = re.compile(r"([\s\S]+) feat\.([\s\S]+)", re.IGNORECASE)
 
 
+def _fixed_title(s):
+    """
+    Title-cases a string, but corrects for apostrophes, e.g., "Don't"
+    instead of "Don'T".
+    """
+    titled_s = s.title()
+    return re.sub(r"(\w)'(\w)", lambda m: m.group(1) + "'" + m.group(2).lower(), titled_s)
+
+
 def move_album_featartists(tagger, metadata, release):
     match = _feat_re.match(metadata["albumartist"])
     if match:
@@ -21,12 +30,12 @@ def move_album_featartists(tagger, metadata, release):
 
 
 def move_track_featartists(tagger, metadata, track, release):
-    title = metadata["title"].title()
+    title = _fixed_title(metadata["title"])
     match = _feat_re.match(metadata["artist"])
     if match:
         metadata["artist"] = match.group(1)
         featured_artist = match.group(2).strip()
-        title += " (feat. " + featured_artist.title() + ")"
+        title += " (feat. " + _fixed_title(featured_artist) + ")"
     metadata["title"] = title
     match = _feat_re.match(metadata["artistsort"])
     if match:
